75aa5e53ad421a6906759d302a2dba5bc5b4b50b
Edvid <spacemastr@gmail.com>
2024-08-29 15:44:05 +0200

    Try a really desperate basePath solution

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/.',
+  basePath: 'https://raw.githubusercontent.com/Edvid/chilldoro/main/docs',
   output: 'export',
 };
 

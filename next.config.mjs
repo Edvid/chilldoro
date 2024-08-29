e2479a3d1591991495f645b05c3935532e5efd27
Edvid <spacemastr@gmail.com>
2024-08-29 15:52:43 +0200

    assetPrefix attempt

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: 'https://raw.githubusercontent.com/Edvid/chilldoro/main/docs',
-  output: 'export',
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js'
+
+const nextConfig = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  /** @type {import('next').NextConfig} */
+  return {
+    assetPrefix: isDev ? undefined : 'https://raw.githubusercontent.com/Edvid/chilldoro/main/docs',
+    output: 'export',
+  }
 };
 
 export default nextConfig;

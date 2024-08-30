e94e601b552fd90f17072c27e778cd864c3257a4
Edvid <spacemastr@gmail.com>
2024-08-30 02:27:25 +0200

    changed time format of <TimeLeft/> to MM:SS

--- _components/timeLeft.tsx
+++ _components/timeLeft.tsx
@@ -2,23 +2,23 @@
 import React from 'react'
 import { TimeInfo } from '~/_utils/types'
 
+function fitToTwo(str: string | number){
+  return ("0" + str).slice(-2);
+}
+
 export default function TimeLeft(props: {timeInfo: TimeInfo} ) {
+  const isActive = props.timeInfo.secondsSinceStartTime <= props.timeInfo.pomodoroType.active * 60;
+  const seconds = isActive ?  Math.ceil(props.timeInfo.pomodoroType.active * 60 - props.timeInfo.secondsSinceStartTime) : Math.ceil(props.timeInfo.pomodoroType.break * 60 - (props.timeInfo.secondsSinceStartTime - props.timeInfo.pomodoroType.active * 60));
+  const sec = seconds % 60;
+  const min = Math.floor(seconds / 60);
+  const time = `${fitToTwo(min)}:${fitToTwo(sec)}`;
   return (
     <div>
-      {
-        props.timeInfo.secondsSinceStartTime <= props.timeInfo.pomodoroType.active * 60 ?
-          <h1
-            className='text-[8em] text-center text-green-700'
-          >
-            {Math.ceil(props.timeInfo.pomodoroType.active * 60 - props.timeInfo.secondsSinceStartTime)}
-          </h1>
-          :
-          <h1
-            className='text-[8em] text-center text-cyan-300'
-          >
-            {Math.ceil(props.timeInfo.pomodoroType.break * 60 - (props.timeInfo.secondsSinceStartTime - props.timeInfo.pomodoroType.active * 60))}
-          </h1>
-      }
+      <h1
+        className={`text-[8em] text-center ${ isActive ? "text-green-700" : "text-cyan-300"}`}
+      >
+        {time}
+      </h1>
     </div>
   )
 }

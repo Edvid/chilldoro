8fe0528786a9167b87671e025713042c05bbfd0b
Edvid <spacemastr@gmail.com>
2024-09-17 12:38:15 +0200

    Change icon for png

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ next build && \
 mv docs/_next docs/next && \
 grep -rl '\/_next' docs | \
 xargs sed -i 's/\/_next/\/next/g' && \
-sed -i 's/href=\"\/favicon.ico/href=\"\/chilldoro\/favicon.ico/g' docs/index.html
+sed -i 's/href=\"\/favicon.png/href=\"\/chilldoro\/favicon.png/g' docs/index.html
